95ae446d0e46163c355ef08fa02a79c846c269e3
王李超 <2958251898@qq.com>
2024-10-28 14:07:01 +0800

    Autosave

--- C/VS Code/educoder/test/test2/level_7.c
+++ C/VS Code/educoder/test/test2/level_7.c
@@ -1,3 +1,19 @@
+/*
+任务描述
+本关任务：实验教材P63 程序设计第(8)题
+输入一个仅有英文大写字母、逗号、句点和空格符组成的英文句子（字符数不超过80个）和正整数k（1<=k<=26），将其中的大写英文字母替换成字母表中该字母开始的第k个字母，其它字符保持不变.
+
+测试说明
+平台会对你编写的代码进行测试：
+
+测试输入：
+ON A CLEAR DAY,YOU CAN SEE FOREVER
+9
+预期输出：
+VWI K MTZIL GIG,CWK VIA MMN ZWDMZM
+
+开始你的任务吧，祝你成功！
+*/
 #include<stdio.h>
 int encryption(char REAL[1000])
 {
