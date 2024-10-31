e2105a84d8e5e6c2ad4b032004f45e3dbe8f3a8b
王李超 <2958251898@qq.com>
2024-10-31 20:21:13 +0800

    Autosave

--- C/VS Code/educoder/Theoretical/Chapter 7/level 7_4/step4_stu.h
+++ C/VS Code/educoder/Theoretical/Chapter 7/level 7_4/step4_stu.h
@@ -2,7 +2,11 @@
 
 void strnCpy(char t[],char s[],int n){
 	/**********  Begin  **********/
-
-
+    int i = 0;
+    for(; i < n; i++)
+    {
+        t[i] = (char)s[i];
+    }
+    t[i] = '\0';
 	/**********  End  **********/
 }
