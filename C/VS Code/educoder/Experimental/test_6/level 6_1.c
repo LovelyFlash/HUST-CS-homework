a5bb210beb7a96f7be16df18f8e57f472c29da98
王李超 <2958251898@qq.com>
2024-11-10 11:19:59 +0800

    Autosave

---
+++ C/VS Code/educoder/Experimental/test_6/level 6_1.c
@@ -0,0 +1,25 @@
+/*
+任务描述
+实验教材P179 6.3.2节 1.程序改错与跟踪调试
+请按教材中的线下完成跟踪调试程序，找出错误并改正，然后提交程序进行评测。
+在题目所给的源程序中，函数strcopy(t,s)的功能是将字符串s复制给字符串t，并返回串t的首地址。
+
+输入输出示例
+输入：
+programming
+language
+输出：
+programming
+language
+
+开始你的任务吧，祝你成功！
+*/
+#include <stdio.h>
+#define MAX 100
+
+int main()
+{
+    int s[MAX], t[MAX];
+    
+    return 0;
+}
