1ba89e643b17b6dd1b2d9e2660298986ff43e0be
王李超 <2958251898@qq.com>
2024-11-12 14:25:03 +0800

    2024.11.12

--- C/VS Code/educoder/Experimental/test_6/level 6_1.c
+++ C/VS Code/educoder/Experimental/test_6/level 6_1.c
@@ -15,11 +15,23 @@ language
 开始你的任务吧，祝你成功！
 */
 #include <stdio.h>
-#define MAX 100
+char *strcopy(char *, const char *);
 
-int main()
+int main(void)
 {
-    int s[MAX], t[MAX];
-    
+    char s1[100], s2[100], *s3;
+    scanf("%99s", s2);
+    strcopy(s1, s2);
+    printf("%s\n", s1);
+    scanf("%99s", s2);
+    s3 = strcopy(s1, s2);
+    printf("%s\n", s3);
     return 0;
+}
+
+char *strcopy(char *t, const char *s)
+{
+    char *start = t;
+    while((*t++ = *s++));
+    return start;
 }
---
+++ C/VS Code/educoder/Experimental/test_6/level 6_4.c
@@ -0,0 +1,26 @@
+/*
+任务描述
+本关任务：实验教材P179 6.3.2节 2.程序完善和修改替换 第（2）题第①问
+通过函数指针和菜单选择来调用库函数实现字符串操作：串复制strcpy、串连接strcat或串分解strtok。请完善该程序
+( 函数strtok的实现和使用参见理论课教材例8.19)
+
+输入输出示例
+输入：
+1
+the more you learn,
+the more you get.
+2
+the more you learn,
+the more you get.
+3
+www.educoder.net
+.
+4
+
+输出：
+the more you get.
+the more you learn,the more you get.
+www
+
+开始你的任务吧，祝你成功！
+*/
