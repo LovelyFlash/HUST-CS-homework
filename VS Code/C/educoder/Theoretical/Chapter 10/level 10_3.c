742168f83662772964e8e86bd8b2649257d7085e
王李超 <2958251898@qq.com>
2024-11-20 14:46:45 +0800

    2024.11.20

---
+++ VS Code/C/educoder/Theoretical/Chapter 10/level 10_3.c
@@ -0,0 +1,44 @@
+/*
+任务描述
+本关任务：现有如表中所示的4个常用的网站网址信息，包括缩略名、全名、url信息。现设计一个结构体struct web,并编写结构体数组获取表中的网址信息，同时将网站信息按照缩略名排序并按照升序输出,最后获取一个缩略名，输出它的URL信息。
+要求：将排序定义成函数。
+
+缩略名	全名	URL
+hkd	华中科技大学	http://www.hust.edu.cn
+bd	百度搜索网站	http://www.baidu.com
+gg	谷歌搜索网站	http://www.google.com
+wy	网易邮箱登录	https://mail.163.com
+测试说明
+平台会对你编写的代码进行测试，若是与预期输出相同，则算通关。
+
+测试输入：
+hkd 华中科技大学 http://www.hust.edu.cn
+bd 百度搜索网站 http://www.baidu.com
+gg 谷歌搜索网站 http://www.google.com
+wy 网易邮箱登录 https://mail.163.com
+gg
+预期输出：
+bd 百度搜索网站 http://www.baidu.com
+gg 谷歌搜索网站 http://www.google.com
+hkd 华中科技大学 http://www.hust.edu.cn
+wy 网易邮箱登录 https://mail.163.com
+http://www.google.com
+
+测试输入：
+hkd 华中科技大学 http://www.hust.edu.cn
+bd 百度搜索网站 http://www.baidu.com
+gg 谷歌搜索网站 http://www.google.com
+wy 网易邮箱登录 https://mail.163.com
+cs
+预期输出：
+bd 百度搜索网站 http://www.baidu.com
+gg 谷歌搜索网站 http://www.google.com
+hkd 华中科技大学 http://www.hust.edu.cn
+wy 网易邮箱登录 https://mail.163.com
+未找到搜寻的网址
+
+要求
+输入格式与输出格式为，同一网站的信息之间以空格分隔，不同网站信息之间以回车分隔。
+
+开始你的任务吧，祝你成功！
+*/
