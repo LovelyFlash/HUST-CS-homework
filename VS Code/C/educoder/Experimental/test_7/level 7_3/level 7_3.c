74d7b882f19c4e8ba0dfb15c831c58dc1a7bde23
王李超 <2958251898@qq.com>
2024-11-20 22:10:41 +0800

    2024.11.20

--- VS Code/C/educoder/Experimental/test_7/level 7_3/level 7_3.c
+++ VS Code/C/educoder/Experimental/test_7/level 7_3/level 7_3.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct s_list{
-    int data;
-    struct s_list *next;
+
+struct intNode{
+    int c;
+    struct intNode *next;
 };
-struct s_list *create_list(struct s_list *headp,int *p);
-int main(void){
-    struct s_list *head=NULL,*p;
-    int s[]={1,2,3,4,5,6,7,8,0};
-    p=create_list(head,s);
-    while (p != NULL){
-        printf("%d\t",p->data);
-        p=p->next;
-    }
-    printf("\n");
-    return 0;  
-}
-struct s_list *create_list(struct s_list *headp,int *p){
-    struct s_list *x, *head = NULL;
-    if(p[0]==0);
-    else {
-        while (*p)
-        {
-            x = (struct s_list *)malloc(sizeof(struct s_list));
-            x -> next= head;
-            x -> data = *(p++);
-            head = x;
-        }
+
+struct intNode *creatlist()
+{
+    struct intNode *head, *p;
+    int x;
+    head = NULL;
+    scanf("%d", &x);
+    while(x != 0)
+    {
+        p = (struct intNode *)malloc(sizeof(struct intNode));
+        p -> c = x;
+        p -> next = head;
+        head = p;
+        scanf("%d", &x);
     }
     return head;
+}
+
+void printList(struct intNode *head)
+{
+    struct intNode *p;
+    p = head;
+    while(p != NULL)
+    {
+        printf("%d\t", p -> c);
+        p = p -> next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    struct intNode *head;
+    head = creatlist();
+    printList(head);
+    return 0;
 }
