22e3be17b2ccbaa6d41c24cfad2e88c486df0227
王李超 <2958251898@qq.com>
2024-11-21 23:00:14 +0800

    Autosave

---
+++ VS Code/C/educoder/Experimental/test_7/level 7_5/level 7_5.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+struct scoreNode{
+    char num[10];
+    char name[10];
+    int score[4];
+    struct scoreNode *next;
+};
+
+struct scoreNode *createlist()
+{
+    struct scoreNode *p, *head, *tail;
+    head = NULL;
+    tail = NULL;
+    char x[10], y[10]; 
+    int z[4];
+    scanf("%s %s %d %d %d %d", x, y, &z[0], &z[1], &z[2], &z[3]);
+}
+
+void fun1()
+{
+
+}
+
+void fun2()
+{
+
+}
+
+void fun3()
+{
+
+}
+
+void fun3_1()
+{
+
+}
+
+void fun3_2()
+{
+
+}
+
+void fun3_3()
+{
+
+}
+
+void fun3_4()
+{
+
+}
+
+void fun4()
+{
+
+}
+
+void fun5()
+{
+
+}
+
+int main()
+{
+
+}
