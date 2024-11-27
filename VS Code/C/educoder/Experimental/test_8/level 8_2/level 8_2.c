8e4692f20880a0597a9cac03b58c2ae104685414
王李超 <2958251898@qq.com>
2024-11-27 16:20:41 +0800

    2024.11.27

--- VS Code/C/educoder/Experimental/test_8/level 8_2/level 8_2.c
+++ VS Code/C/educoder/Experimental/test_8/level 8_2/level 8_2.c
@@ -1,20 +1,34 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(int argc, char* argv[])
 {
 	FILE * fp;
-	if(argc!=2){
+	if(argc!=3)
+    {
 		printf("Arguments error!\n");
 		exit(-1);
 	}
-	if((fp=fopen(argv[1],"r"))==NULL){      /* fp指向filename */
+	if((fp=fopen(argv[1],"r"))==NULL)
+    {      /* fp指向filename */
 		printf("Can't open %s file!\n", argv[1]);
 		exit(-1);
 	}
-    int ch;
+    int ch, i = 1;
+    printf("%d ", i++);
     while ((ch = fgetc(fp)) != EOF)
     {
-    	putchar(ch);            /* 向显示器中写从filename中读取的字符 */ 
+        if((i-1)%5 == 0)
+        {
+            char c = getchar();
+        }
+        if((char)ch == '\n')
+    	{
+            putchar(ch);            /* 向显示器中写从filename中读取的字符 */ 
+            printf("%d ", i++);
+        }
+        else
+            putchar(ch);
     }    
 
 	fclose(fp);                        /* 关闭filename */ 
