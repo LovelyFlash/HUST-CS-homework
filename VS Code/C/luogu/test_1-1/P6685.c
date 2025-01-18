31134055c4689cd3007f777191ad02bc2ea65244
WLC <2958251898@qq.com>
2025-01-18 23:01:58 +0800

    winter_practice

--- VS Code/C/luogu/test_1-1/P6685.c
+++ VS Code/C/luogu/test_1-1/P6685.c
@@ -1,33 +1,72 @@
+// #include <stdio.h>
+// #include <math.h>
+
+// int main()
+// {
+//     long n, m;
+//     scanf("%ld %ld", &n, &m);
+//     long i, right = 0, left = n;
+//     while (right != left && right + 1 != left)
+//     {
+//         i = (left + right) / 2;
+//         long long p = 1;
+//         for (long k = 0; k < m; k++) // 手动计算 mid^m
+//         {
+//             p *= i;
+//             if (p > n) // 提前终止，防止溢出
+//             {
+//                 left = i;
+//                 break;
+//             }
+//         }
+        
+//         if (p < n)
+//             right = i;
+//         else if (p == n)
+//             break;
+//         else
+//             left = i;
+//     }
+//     i = (left + right) / 2;
+//     printf("%ld", i);
+//     return 0;
+// }
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
     long n, m;
     scanf("%ld %ld", &n, &m);
-    long i, right = 0, left = n;
-    while (right != left && right + 1 != left)
+
+    long left = 1, right = n, result = 0;
+
+    while (left <= right)
     {
-        i = (left + right) / 2;
-        long long p = 1;
-        for (long k = 0; k < m; k++) // 手动计算 mid^m
+        long mid = left + (right - left) / 2;
+        long long power = 1; // 防止溢出
+        int valid = 1;
+
+        for (long i = 0; i < m; i++) // 手动计算 mid^m
         {
-            p *= i;
-            if (p > n) // 提前终止，防止溢出
+            power *= mid;
+            if (power > n) // 提前终止，防止溢出
             {
-                left = i;
+                valid = 0;
                 break;
             }
         }
-        
-        if (p < n)
-            right = i;
-        else if (p == n)
-            break;
+
+        if (valid && power <= n) // mid^m <= n
+        {
+            result = mid; // 更新结果
+            left = mid + 1;
+        }
         else
-            left = i;
+        {
+            right = mid - 1;
+        }
     }
-    i = (left + right) / 2;
-    printf("%ld", i);
+
+    printf("%ld", result);
     return 0;
 }
