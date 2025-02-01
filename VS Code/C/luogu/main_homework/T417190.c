96d8a3af456696c4e576a7790f26dd140a513950
王李超 <2958251898@qq.com>
2025-02-01 15:22:17 +0800

    main_homework

--- VS Code/C/luogu/main_homework/T417190.c
+++ VS Code/C/luogu/main_homework/T417190.c
@@ -3,20 +3,19 @@
 
 long long n, q, x, y;
 
-// long long cmpfunc(const void *a, const void *b)
-// {
-//     return (*(long long *)a - *(long long *)b);
-// }
+long long cmpfunc(const void *a, const void *b)
+{
+    return (*(long long *)a - *(long long *)b);
+}
 
 int main()
 {
     scanf("%lld %lld", &n, &q);
-    long long equal[n][3], answer[n], num[200000] = {0};
+    long long equal[n][3], answer[n];
     for (long long i = 0; i < n; i++)
     {
         scanf("%lldx%lld=%lld", &equal[i][0], &equal[i][1], &equal[i][2]);
         answer[i] = (equal[i][2] - equal[i][1]) / equal[i][0];
-        num[answer[i]] = 1;
     }
     for (long long i = 0; i < q; i++)
     {
@@ -24,7 +23,7 @@ int main()
         long long count = 0;
         for (long long i = x; i < y + 1; i++)
         {
-            if (num[i])
+            if (bsearch(&i,answer,n,sizeof(int),cmpfunc))
                 count++;
         }
         printf("%lld\n", count);
