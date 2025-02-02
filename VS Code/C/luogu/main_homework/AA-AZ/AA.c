e2e8ac912f4fb1bbd5438aaa3f4e421d76276f44
王李超 <2958251898@qq.com>
2025-02-02 19:12:48 +0800

    file_cleaning

--- VS Code/C/luogu/main_homework/AA-AZ/AA.c
+++ VS Code/C/luogu/main_homework/AA-AZ/AA.c
@@ -6,7 +6,7 @@ int cmpfunc(const void *a, const void *b)
     return (*(int *)a - *(int *)b);
 }
 
-int mian()
+int main()
 {
     int N, B, sum = 0, k;
     scanf("%d %d", &N, &B);
---
+++ VS Code/C/luogu/main_homework/AA-AZ/AC.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+long long *multiply(long long *mat,long long n);
+long long *qmult(long long num);
+
+int main(){
+    long long n,f1,f2,a,b,c;
+    
+}
