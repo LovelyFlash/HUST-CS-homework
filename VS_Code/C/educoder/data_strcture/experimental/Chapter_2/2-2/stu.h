6d0fc7ab5574bb56d73dbc85f1d70a3db75a19cb
王李超 <2958251898@qq.com>
2025-04-08 19:23:41 +0800

    HOMEWORK
    Signed-off-by: 王李超 <2958251898@qq.com>

--- VS_Code/C/educoder/data_strcture/experimental/Chapter_2/2-2/stu.h
+++ VS_Code/C/educoder/data_strcture/experimental/Chapter_2/2-2/stu.h
@@ -3,7 +3,19 @@ status DestroyList(LinkList &L)
 {
     // 请在这里补充代码，完成本关任务
     /********** Begin *********/
-
-
+    if (L != NULL)
+    {
+        LinkList p = L->next, q;
+        while (p != NULL)
+        {
+            q = p->next;
+            free(p);
+            p = q;
+        }
+        free(L);
+        L = NULL;
+        return OK;
+    }
+    else return INFEASIBLE;
     /********** End **********/
 }
