9bea75e2525572dfc3b8ff96fd01b941f95c0b15
王李超 <2958251898@qq.com>
2025-04-16 09:17:02 +0800

    homework
    Signed-off-by: 王李超 <2958251898@qq.com>

--- VS_Code/C/educoder/data_strcture/experimental/Chapter_3/3-4/stu.h
+++ VS_Code/C/educoder/data_strcture/experimental/Chapter_3/3-4/stu.h
@@ -1,6 +1,5 @@
-status CreateBiTree(BiTree &T,TElemType definition[])
-/*根据带空枝的二叉树先根遍历序列definition构造一棵二叉树，将根节点指针赋值给T并返回OK，
-如果有相同的关键字，返回ERROR。此题允许通过增加其它函数辅助实现本关任务*/
+BiTNode* LocateNode(BiTree T,KeyType e)
+//查找结点
 {
     // 请在这里补充代码，完成本关任务
     /********** Begin *********/
