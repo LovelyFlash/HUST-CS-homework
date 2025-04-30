dba25e5d2d7360db59d93ea25b2a032ea713754d
王李超 <2958251898@qq.com>
2025-04-30 21:11:26 +0800

    homework

--- VS_Code/C/educoder/data_strcture/experimental/Chapter_3/main03.cpp
+++ VS_Code/C/educoder/data_strcture/experimental/Chapter_3/main03.cpp
@@ -1,152 +1,127 @@
 #include <bits/stdc++.h>
 #include <windows.h>
-#include "fun.c"
-#include "def.h"
+#include "def.h"  // 相关数据类型的定义
+#include "func.h" // 相关功能的定义
 
-using namespace std;
-
-class T
-{
-private:
-    int data;
-    int key;
-
-public:
-    T(int e)
-    {
-        data = e;
-    }
-    int getData()
-    {
-        return data;
-    }
-}
-
-template <class T>
-class BiTree
-{
-private:
-    T data;
-    BiTree<T> *lchild, *rchild;
-
-public:
-    BiTree(T e)
-    {
-        data = e;
-        lchild = NULL;
-        rchild = NULL;
-    }
-    
-    
-}
-
-int
-main()
+int main(void)
 {
     SetConsoleOutputCP(65001); // 设置控制台输出编码为UTF-8
+    BiTree T;
+    List Trees; // 定义一个二叉树数组，最多存储10个二叉树
+    Trees.elem = (BiTree *)malloc(sizeof(BiTree));
+    Trees.length = 0;                // 初始化二叉树数组长度
+    Trees.listsize = LIST_INIT_SIZE; // 初始化二叉树存储空间大小
+    strcpy(Trees.name[0], "二叉树1"); // 初始化二叉树名称
     static int num = 0;
     int op = 1;
     while (op)
     {
         printf("\n\n");
-        printf("      链表操作菜单 \n");
+        printf("      二叉树操作菜单 \n");
         printf("===============================================\n");
         printf("  基本操作：\n");
-        printf("    1. 初始化链表           2. 销毁链表\n");
-        printf("    3. 清空链表             4. 判断链表是否为空\n");
-        printf("    5. 获取链表长度\n");
+        printf("    1. 创建二叉树           2. 销毁二叉树\n");
+        printf("    3. 清空二叉树           4. 判定空二叉树\n");
+        printf("    5. 求二叉树深度\n");
         printf("-----------------------------------------------\n");
-        printf("  元素操作：\n");
-        printf("    6. 获取指定位置元素       7. 查找元素位置\n");
-        printf("    8. 获取前驱元素           9. 获取后继元素\n");
-        printf("   10. 插入元素              11. 删除元素\n");
+        printf("  结点操作：\n");
+        printf("    6. 查找结点             7. 结点赋值\n");
+        printf("    8. 获得兄弟结点         9. 插入结点\n");
+        printf("   10. 删除结点\n");
         printf("-----------------------------------------------\n");
         printf("  遍历操作：\n");
-        printf("   12. 遍历链表\n");
+        printf("   11. 前序遍历            12. 中序遍历\n");
+        printf("   13. 后序遍历            14. 按层遍历\n");
         printf("-----------------------------------------------\n");
-        printf("  附加操作：\n");
-        printf("   13. 链表翻转             14. 删除链表的倒数第n个结点\n");
-        printf("   15. 链表排序             16. 保存链表到文件\n");
-        printf("   17. 从文件加载链表\n");
+        printf("  附加功能：\n");
+        printf("   15. 最大路径和          16. 最近公共祖先\n");
+        printf("   17. 翻转二叉树          18. 保存二叉树到文件\n");
+        printf("   19. 从文件加载二叉树\n");
         printf("-----------------------------------------------\n");
-        printf("  多链表管理：\n");
-        printf("   18. 添加链表           19. 删除链表\n");
-        printf("   20. 定位链表           21. 修改链表名称\n");
-        printf("   22. 输出所有链表名称\n");
+        printf("  多二叉树管理：\n");
+        printf("   20. 添加二叉树          21. 删除二叉树\n");
+        printf("   22. 定位二叉树          23. 修改二叉树名称\n");
+        printf("   24. 输出所有二叉树名称\n");
         printf("    0. 退出程序\n");
         printf("===============================================\n");
-        printf("  正在操作的链表名称：%s\n", Lists.name[num - 1]);
-        printf("  请选择你的操作 [0 ~ 22]: ");
+        printf("  正在操作的二叉树名称：%s\n", Trees.name[num - 1]);
+        printf("  请选择你的操作 [0 ~ 24]: ");
         scanf("%d", &op);
 
         // 输出对应的功能名称
         switch (op)
         {
         case 1:
-            printf("\t初始化链表\n");
+            printf("\t创建二叉树\n");
             break;
         case 2:
-            printf("\t销毁链表\n");
+            printf("\t销毁二叉树\n");
             break;
         case 3:
-            printf("\t清空链表\n");
+            printf("\t清空二叉树\n");
             break;
         case 4:
-            printf("\t判断链表是否为空\n");
+            printf("\t判定空二叉树\n");
             break;
         case 5:
-            printf("\t获取链表长度\n");
+            printf("\t求二叉树深度\n");
             break;
         case 6:
-            printf("\t获取指定位置元素\n");
+            printf("\t查找结点\n");
             break;
         case 7:
-            printf("\t查找元素位置\n");
+            printf("\t结点赋值\n");
             break;
         case 8:
-            printf("\t获取前驱元素\n");
+            printf("\t获得兄弟结点\n");
             break;
         case 9:
-            printf("\t获取后继元素\n");
+            printf("\t插入结点\n");
             break;
         case 10:
-            printf("\t插入元素\n");
+            printf("\t删除结点\n");
             break;
         case 11:
-            printf("\t删除元素\n");
+            printf("\t前序遍历\n");
             break;
         case 12:
-            printf("\t遍历链表\n");
+            printf("\t中序遍历\n");
             break;
         case 13:
-            printf("\t链表翻转\n");
+            printf("\t后序遍历\n");
             break;
         case 14:
-            printf("\t删除链表的倒数第n个结点\n");
+            printf("\t按层遍历\n");
             break;
         case 15:
-            printf("\t链表排序\n");
+            printf("\t最大路径和\n");
             break;
         case 16:
-            printf("\t保存链表到文件\n");
+            printf("\t最近公共祖先\n");
             break;
         case 17:
-            printf("\t从文件加载链表\n");
+            printf("\t翻转二叉树\n");
             break;
         case 18:
-            printf("\t添加链表\n");
+            printf("\t保存二叉树到文件\n");
             break;
         case 19:
-            printf("\t删除链表\n");
+            printf("\t从文件加载二叉树\n");
             break;
         case 20:
-            printf("\t定位链表\n");
+            printf("\t添加二叉树\n");
             break;
         case 21:
-            printf("\t修改链表名称\n");
+            printf("\t删除二叉树\n");
             break;
         case 22:
-            printf("\t输出所有线性表名称\n");
+            printf("\t定位二叉树\n");
+            break;
+        case 23:
+            printf("\t修改二叉树名称\n");
+            break;
+        case 24:
+            printf("\t输出所有二叉树名称\n");
             break;
         case 0:
             printf("\t退出程序\n");
@@ -155,6 +130,10 @@ main()
             printf("\t输入错误，请重新输入！\n");
             break;
         }
+        printf("\t请按回车键继续...");
+        getchar();
+        getchar();
     }
+    printf("欢迎下次再使用本系统！\n");
     return 0;
-} // end of main
+}
