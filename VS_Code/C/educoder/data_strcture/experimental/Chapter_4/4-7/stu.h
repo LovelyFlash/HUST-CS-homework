a0d9e0a68ca0ba1910039c37ffdd3b0bebae8de1
WLC <2958251898@qq.com>
2025-05-03 14:50:39 +0800

    homework

--- VS_Code/C/educoder/data_strcture/experimental/Chapter_4/4-7/stu.h
+++ VS_Code/C/educoder/data_strcture/experimental/Chapter_4/4-7/stu.h
@@ -1,9 +1,17 @@
-status InsertVex(ALGraph &G,VertexType v)
-//在图G中插入顶点v，成功返回OK,否则返回ERROR
+status InsertVex(ALGraph &G, VertexType v)
+// 在图G中插入顶点v，成功返回OK,否则返回ERROR
 {
     // 请在这里补充代码，完成本关任务
     /********** Begin *********/
-    G.vertices = (AdjList)realloc(G.vertices, (G.vexnum + 1) * sizeof(VNode));
+    for (int i = 0; i < G.vexnum; i++)
+    {
+        if (G.vertices[i].data.key == v.key)
+            return ERROR;
+    }
+    if (G.vexnum >= MAX_VERTEX_NUM)
+        return ERROR;
+
+    G.vertices[G.vexnum] = *(VNode *)malloc(sizeof(VNode));
     if (G.vertices == NULL)
         return ERROR;
     G.vertices[G.vexnum].data = v;
--- VS_Code/C/educoder/data_strcture/experimental/Chapter_4/4-8/stu.h
+++ VS_Code/C/educoder/data_strcture/experimental/Chapter_4/4-8/stu.h
@@ -1,9 +1,68 @@
-status DeleteVex(ALGraph &G,KeyType v)
-//在图G中删除关键字v对应的顶点以及相关的弧，成功返回OK,否则返回ERROR
+void free0(void *p);
+
+status DeleteVex(ALGraph &G, KeyType v)
+// 在图G中删除关键字v对应的顶点以及相关的弧，成功返回OK,否则返回ERROR
 {
     // 请在这里补充代码，完成本关任务
     /********** Begin *********/
-
-
+    if (G.vexnum == 1)
+        return ERROR;
+    for (int i = 0; i < G.vexnum; i++)
+    {
+        if (G.vertices[i].data.key == v)
+        {
+            ArcNode *p = G.vertices[i].firstarc;
+            while (p != NULL)
+            {
+                ArcNode *q = p;
+                p = p->nextarc;
+                free0(q);
+            }
+            for (int j = i; j < G.vexnum - 1; j++)
+            {
+                G.vertices[j] = G.vertices[j + 1];
+            }
+            for (int j = 0; j < G.vexnum - 1; j++)
+            {
+                if (G.vertices[j].firstarc != NULL)
+                {
+                    ArcNode *p = G.vertices[j].firstarc;
+                    ArcNode *q = NULL;
+                    while (p != NULL)
+                    {
+                        if (p->adjvex == i)
+                        {
+                            if (q == NULL)
+                            {
+                                G.vertices[j].firstarc = p->nextarc;
+                                G.arcnum--;
+                            }
+                            else
+                            {
+                                q->nextarc = p->nextarc;
+                                G.arcnum--;
+                            }
+                            free0(p);
+                            break;
+                        }
+                        q = p;
+                        p = p->nextarc;
+                    }
+                    ArcNode *p2 = G.vertices[j].firstarc;
+                    ArcNode *q2 = NULL;
+                    while (p2 != NULL)
+                    {
+                        if (p2->adjvex > i)
+                            p2->adjvex--;
+                        q2 = p2;
+                        p2 = p2->nextarc;
+                    }
+                }
+            }
+            G.vexnum--;
+            return OK;
+        }
+    }
+    return ERROR;
     /********** End **********/
 }
