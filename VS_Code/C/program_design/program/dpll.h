814b3651d6c02932374020b09d4c9ebdf9bc9235
王李超 <2958251898@qq.com>
2025-09-04 23:33:55 +0800

    homework

--- VS_Code/C/program_design/program/dpll.h
+++ VS_Code/C/program_design/program/dpll.h
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #ifndef _DPLL_
 #define _DPLL_
-
+int MAX = 730;
 int cmp_1(const void *a, const void *b)
 {
     return a > b;
@@ -10,7 +10,6 @@ int cmp_1(const void *a, const void *b)
 class Sentence
 {
 private:
-    int MAX = 730;
     int *arr;
 
 public:
@@ -20,48 +19,61 @@ public:
         for (int i = 0; i < MAX; i++)
             arr[i] = 0;
     }
+
+    // 拷贝构造函数
+    Sentence(const Sentence &other)
+    {
+        arr = new int[MAX];
+        for (int i = 0; i < MAX; i++)
+            arr[i] = other.arr[i];
+    }
+
+    void out()
+    {
+        for (int i = 0; i < MAX; i++)
+            std::cout << i << ' ' << arr[i] << ' ';
+        std::cout << '\n';
+    }
+
     int operator[](int n)
     {
-        return arr[n];
+        return arr[abs(n)];
     }
     void assign(int n)
 
     // 标识对应文字存在性以及正负性
     {
-        if (n > 0)
-            arr[n] = 1;
-        else if (n < 0)
-            arr[n] = -1;
+        arr[abs(n)] = n / abs(n);
         arr[0]++;
     }
 
     void del_var(int n)
     // 依据单子句规则修改句子
     {
-        if (arr[n] == 1)
+        if (arr[abs(n)] == n / abs(n))
             arr[0] = -1; // 标识该句子已被删除
-        else if (arr[n] == -1)
+        else if (arr[abs(n)] == -n / abs(n))
         {
-            arr[n] = 0;
+            arr[abs(n)] = 0;
             arr[0]--; // 标识该句子文字数量减少
         }
     }
 
     void add(int n)
     { // 用于记录文字出现次数
-        arr[n]++;
+        arr[abs(n)]++;
     }
 
     void minus(int n)
     { // 用于记录文字出现次数
-        arr[n]--;
+        arr[abs(n)]--;
     }
 
     int findFirstVar()
     // 找到该句子中第一个文字
     {
-        int i = 0;
-        while (arr[i])
+        int i = 1;
+        while (arr[i] == 0)
             i++;
         return i;
     }
@@ -78,11 +90,23 @@ int cmp_adapter(const void *a, const void *b)
 struct CNF
 {
     int var_num = -1, clause = -1, var;
-    Sentence *sentence;
+    Sentence *sentence = new Sentence[1];
+    CNF() {
+    };
 
+    CNF(const CNF &other)
+    {
+        var_num = other.var_num;
+        var = other.var;
+        clause = other.clause;
+        sentence = new Sentence[other.clause + 1];
+        for (int i = 0; i < other.clause + 1; i++)
+            sentence[i] = Sentence(other.sentence[i]);
+    }
     CNF(const char *filename)
     {
         FILE *file = fopen(filename, "r");
+        int clause_num = 1;
         while (!feof(file))
         {
             char tem[100];
@@ -101,6 +125,10 @@ struct CNF
                 try
                 {
                     sscanf(line, "%s%s%d%d", &msg, tem, &var_num, &clause);
+                    delete[] sentence; // 释放原有空间，防止内存泄漏
+                    MAX = var_num + 1;
+                    sentence = new Sentence[clause + 1];
+                    // 第一个数组用于存储文字出现次数
                 }
                 catch (const std::exception &e)
                 {
@@ -112,7 +140,45 @@ struct CNF
                     printf("CNF文件错误：文件信息有误");
                     exit(-1);
                 }
-                break;
+            }
+            else if (line[0] >= '0' && line[0] <= '9' || line[0] == '-')
+            {
+                if (var_num == -1 || clause == -1)
+                {
+                    printf("CNF文件错误：文件信息缺失");
+                    exit(-1);
+                }
+                int i = 0;
+                char *p = line;
+                while (1)
+                {
+                    sscanf(p, "%d", &var);
+                    if (var > var_num || var < -var_num)
+                    {
+                        printf("CNF文件错误：文字编号超出范围");
+                        exit(-1);
+                    }
+                    if (var == 0)
+                        break;
+                    if (var == -1307)
+                    {
+                        sentence[clause_num].assign(var); // 记录句子
+                        sentence[0].add(var);             // 记录文字出现次数
+                    }
+                    else
+                    {
+                        sentence[clause_num].assign(var); // 记录句子
+                        sentence[0].add(var);             // 记录文字出现次数
+                    }
+                    while (*p != ' ' && *p != '\n' && *p != '\0')
+                        p++;
+                    if (*p == '\n' || *p == '\0')
+                        break;
+                    p++;
+                }
+                clause_num++;
+                if (clause_num >= clause + 1)
+                    break;
             }
             else
             {
@@ -120,44 +186,34 @@ struct CNF
                 exit(-1);
             }
         }
-
-        sentence = new Sentence[clause + 1];
-        int num = 1;
-        while (!feof(file))
-        {
-            fscanf(file, "%d", var);
-            if (var)
-            {
-                sentence[num].assign(var);
-                sentence[0].add(var); // 增加对应文字出现次数
-            }
-            else
-                num++;
-        }
     }
     void sort()
     {
-        qsort(sentence, clause, sizeof(Sentence), cmp_adapter);
+        qsort(sentence + 1, clause, sizeof(Sentence), cmp_adapter);
     }
     int var_exist()
     // 判断是否存在单子句
     {
-        for (int i = 0; i < clause; i++)
+        for (int i = 1; i < clause + 1; i++)
             if (sentence[i][0] == 1)
                 return 1;
         return 0;
     }
-    CNF find_maxvar(CNF S)
-    // 找到出现次数最多的文字,并将其作为单子句添加到整体中
+    int find_maxvar(CNF &S)
+    // 找到出现次数最多的文字,返回该文字索引
     {
         int max = -1;
         for (int i = 0; i < var_num; i++)
             if (sentence[0][i] > max)
                 max = i;
+
+        CNF t = CNF(S);
         clause++;
-        sentence = (Sentence *)realloc(sentence, (clause + 1) * sizeof(Sentence)); // 分配空间增加句子
-        sentence[clause].assign(max);
-        return S;
+        sentence = new Sentence[clause + 1];
+        for (int i = 0; i < clause; i++)
+            sentence[i] = t.sentence[i];
+
+        return max;
     }
     void del_var(int line, int var)
     // 删除文字的同时，减少对应的文字出现次数
@@ -165,16 +221,20 @@ struct CNF
         sentence[line].del_var(var);
         sentence[0].minus(var);
     }
-    CNF back(CNF S)
-    // 与find_maxvar功能相似，添加的是否命题单子句
+
+    CNF if_true(CNF &S, int max)
+    // 假设选取变元为真
     {
-        int max = -1;
-        for (int i = 0; i < var_num; i++)
-            if (sentence[0][i] > max)
-                max = i;
-        clause++;
-        sentence = (Sentence *)realloc(sentence, (clause + 1) * sizeof(Sentence));
+        sentence[clause].assign(max);
+        sentence[0].add(max);
+        return S;
+    }
+
+    CNF if_false(CNF &S, int max)
+    // 假设选取变元为假
+    {
         sentence[clause].assign(-max);
+        sentence[0].add(-max);
         return S;
     }
 };
@@ -182,25 +242,43 @@ struct CNF
 bool DPLL(CNF S)
 {
     S.sort();
+    // if (S.clause)
+    //     for (int i = 0; i < S.clause + 1; i++)
+    //         S.sentence[i].out();
     while (S.var_exist())
-        for (int i = 0; S.sentence[i][0] <= 1; i++)
+        for (int i = 1; S.sentence[i][0] <= 1; i++)
+        {
             if (S.sentence[i][0] == 1) // 找到所有单子句并操作
             {
                 int k = S.sentence[i].findFirstVar(); // 找到单子句中的文字
-                for (int j = i; j < S.clause; j++)
+                for (int j = i; j < S.clause + 1; j++)
                 {
                     S.del_var(j, k);           // 删除文字
                     if (S.sentence[j][0] == 0) // 出现空子句即说明无结果
                         return false;
+                    else if (S.sentence[j][0] == -1)
+                        // 若删除句子则减少该句中的文字出现次数
+                        for (int m = 1; m <= S.var_num; m++)
+                            if (S.sentence[j][m] && m != k)
+                                S.sentence[0].minus(m);
                 }
                 S.sort();
-                if (S.sentence[S.clause - 1][0] == -1) // 若排序后文字数量最大的句子已被删除，则说明有解
+
+                if (S.sentence[S.clause][0] == -1) // 若排序后文字数量最大的句子已被删除，则说明有解
+                {
+
+                    for (int i = 0; i < S.clause + 1; i++)
+                        S.sentence[i].out();
                     return true;
+                }
             }
-    CNF tem = S;
-    if (DPLL(S.find_maxvar(S)) > 0)
+        }
+
+    CNF tem = CNF(S);
+    int max = S.find_maxvar(S);
+    if (DPLL(S.if_true(S, max)) > 0)
         return true;
-    return DPLL(tem.back(tem));
+    return DPLL(tem.if_false(tem, max));
 }
 
 bool DPLL(const char *filename)
--- VS_Code/C/program_design/program/main.cpp
+++ VS_Code/C/program_design/program/main.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 using namespace std::filesystem;
 
-int main() {
+int main()
+{
     system("chcp 65001");
     // path root("D:\\desktop\\file\\Code\\VS_Code\\C\\program_design\\program");
     // string ext = ".cnf"; // 你要查找的后缀名
@@ -18,6 +19,9 @@ int main() {
     //     }
     // }
 
-    cout << DPLL("D:\\desktop\\file\\Code\\VS_Code\\C\\program_design\\program\\SAT_test\\false_sample\\eu-rand_net60-25-10.shuffled-3000.cnf");
+    // cout << DPLL("D:\\desktop\\file\\Code\\VS_Code\\C\\program_design\\program\\SAT_test\\true.cnf");
+    cout<<DPLL("D:\\desktop\\file\\Code\\VS_Code\\C\\program_design\\program\\SAT_test\\false_sample\\eu-rand_net60-25-10.shuffled-3000.cnf");
+    // cout << DPLL("D:\\desktop\\file\\Code\\VS_Code\\C\\program_design\\program\\SAT_test\\false_sample\\tst_v10_c100.cnf");
+
     return 0;
 }
